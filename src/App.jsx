942cfe644b01f84e48b0bdf976be964c918b5261
lucariggi92 <riggiluca92@gmail.com>
2026-05-19 18:40:01 +0200

    FIX: app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
-import Header from "./components/Header"
-import Footer from "./components/Footer"
-import {Outlet} from "react-router-dom"
+import HomePage from './pages/HomePage'
+import AppLayout from './layouts/AppLayout'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import './App.css'
 
 function App() {
- 
+  const [count, setCount] = useState(0)
 
   return (
     <>
-     <Header/>
-     <Outlet/>
-     <Footer/>
+      <BrowsRouter>
+
+        <Routes>
+          <Route element={<AppLayout/>}>
+            <Route element={<HomePage/>} path="/" />
+          </Route>
+        </Routes>
+      </BrowsRouter>
+
     </>
   )
 }
---
+++ src/layouts/AppLayout.jsx
@@ -0,0 +1,17 @@
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import {Outlet} from "react-router-dom"
+
+export default function AppLayout() {
+ 
+
+  return (
+    <>
+     <Header/>
+     <Outlet/>
+     <Footer/>
+    </>
+  )
+}
+
+
